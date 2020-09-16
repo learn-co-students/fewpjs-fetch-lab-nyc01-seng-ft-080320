adfc6f2aa89abb1aa40d4e543557bd3174992f32
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-09-16 18:01:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
-
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(json){
+       renderBooks(json)
+    })
 }
 
 function renderBooks(books) {
@@ -13,4 +19,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+    // getPost()
 })
